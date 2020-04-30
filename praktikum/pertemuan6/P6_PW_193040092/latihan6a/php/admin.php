8dc239ea582ba6b354d34431b0fd655ea39d7d24
Muhamad-Rifqi <mrifqims13@gmail.com>
2020-04-30 21:52:40 +0700

    Menambahkan Admin.php di Lat6a

---
+++ praktikum/pertemuan6/P6_PW_193040092/latihan6a/php/admin.php
@@ -0,0 +1,46 @@
+<?php
+require 'function.php';
+$makanan = query("SELECT * FROM makanan");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Admin</title>
+	<link rel="stylesheet" type="text/css" href="../asset/css/styleadmin.css">
+</head>
+<body>
+  <div class="container">
+  	<table border="1" cellpadding="13" cellspacing="0">
+  		<tr>
+			<th>#</th>
+			<th>Opsi</th>
+			<th>Gambar</th>
+			<th>Nama</th> 	
+			<th>Asal</th>
+			<th>Deskripsi</th>
+			<th>Harga</th>
+		</tr>
+
+		<?php $i=1; ?>
+		<?php foreach ($makanan as $m) : ?>
+
+		<div class="tabel">
+			<tr>
+				<td><?=$i;?></td>
+				<td>
+					<a href=""><button>Ubah</button></a>
+					<a href=""><button>Hapus</button></a>
+				</td>
+				<td><img src="../asset/img/<?= $m['Gambar']; ?>"></td>
+				<td><?= $m['Nama']; ?></td>
+				<td><?= $m['Asal']; ?></td>
+				<td><?= $m['Deskripsi']; ?></td>
+				<td> Rp <?= $m['Harga']; ?></td>
+			</tr>
+			<?php $i++; ?>
+		<?php endforeach; ?>
+		</div>
+  	</table>
+</body>
+</html>
