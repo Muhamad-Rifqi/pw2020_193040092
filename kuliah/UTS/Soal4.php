e80101764a31f13b7a238526befd761de5c5a961
Muhamad-Rifqi <mrifqims13@gmail.com>
2020-04-30 14:50:25 +0700

    Menambahkan Soal4.php UTS

---
+++ kuliah/UTS/Soal4.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Document</title>
+</head>
+<body>
+
+	<h1>Halaman Login</h1>
+
+	<form method="post" action="Soal4a.php">
+		<ul>
+			<li>
+				<label for="username">Username :</label>
+				<input type="text" name="username" id="username">
+			</li>
+
+			<li>
+				<label for="password">Password :</label>
+				<input type="password" name="password" id="password">
+			</li>
+
+			<li>
+				<button>Login</button>
+			</li>
+		</ul>
+		
+	</form>
+</body>
+</html>
---
+++ kuliah/UTS/Soal4a.php
@@ -0,0 +1,20 @@
+<?php
+$un = $_REQUEST["username"];
+$pwd = $_REQUEST["password"];
+
+if ($un == "admin" and $pwd = "12345") {
+	print("Selamat Datang Admin");
+} else{
+	print("Username/Password Salah");
+}
+
+
+
+
+
+// print($un);
+// print($pwd);
+
+// print ("respond page");
+// var_dump($_REQUEST);
+?>
