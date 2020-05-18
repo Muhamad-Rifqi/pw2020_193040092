a5fcb920c5163f8809a3462a598e66239e864f25
Muhamad-Rifqi <mrifqims13@gmail.com>
2020-05-18 14:59:25 +0700

    Update index.php

--- praktikum/pertemuan6/P6_PW_193040092/tugas/index.php
+++ praktikum/pertemuan6/P6_PW_193040092/tugas/index.php
@@ -1,7 +1,10 @@
 <?php
     require 'php/function.php';
+    $makanan = query("SELECT * FROM makanan");
 
-    $makanan = query("SELECT * FROM makanan")
+    if (isset($_GET['cari'])) {
+        $makanan = cari($_GET['keyword']);
+        }
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +14,20 @@
 </head>
 <body>
 <div class="container">
+        <form action="" method="get">
+		  <input type="text" name="keyword" autofocus>
+		  <button type="submit" name="cari">Cari!</button>
+		</form>
+
+    <?php if (empty($makanan)) : ?>
+	<tr>
+		<td colspan="7">
+		<h1>Data tidak di temukan</h1>
+		</td>
+	</tr>
+
+	<?php else : ?>
+    <?php $i=1; ?>
     <?php foreach ($makanan as $row) : ?>
         <p class="nama">
             <a href="php/detail.php?id=<?= $row['id'] ?>">
@@ -18,6 +35,9 @@
             </a>
         </p>
     <?php endforeach; ?>
+    <?php endif; ?>
+
+    <a href="php/admin.php"><button>Halaman Admin</button></a>
 </div>
 </body>
 </html>
