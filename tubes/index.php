7d4a30a4fc0f8b5e201d70f771183497e8009ab6
Muhamad-Rifqi <mrifqims13@gmail.com>
2020-05-21 00:23:21 +0700

    Tubes

--- tubes/index.php
+++ tubes/index.php
@@ -9,15 +9,27 @@
 
 <!DOCTYPE html>
 <html>
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Makanan Khas Nusantara</title>
+    <!-- <link rel="stylesheet" type="text/css" href="css/style.css"> -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
 </head>
-<body>
-<div class="container">
-        <form action="" method="get">
-		  <input type="text" name="keyword" autofocus>
-		  <button type="submit" name="cari">Cari!</button>
-		</form>
+<body class="bg-warning">
+    <div class="container bg-light">
+
+    <!-- Bagian Navbar -->
+        <nav class="navbar navbar-light bg-warning">
+    <a class="navbar-brand" href="index.php">Makanan Khas Nusantara</a>
+    <form action="" method="get" class="form-inline">
+        <input class="form-control mr-sm-2" type="text" name="keyword" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light my-2 my-sm-0 bg-secondary" type="submit" name="cari">Search</button>
+    </form>
+    </nav>
+            <!-- <form action="" method="get">
+            <input type="text" name="keyword" autofocus>
+            <button type="submit" name="cari" class="btn btn-secondary">Cari!</button>
+            </form> -->
 
     <?php if (empty($makanan)) : ?>
 	<tr>
@@ -38,6 +50,6 @@
     <?php endif; ?> 
 
     <a href="php/login.php"><button>Masuk Halaman Admin</button></a>
-</div>
+    </div>
 </body>
 </html>
