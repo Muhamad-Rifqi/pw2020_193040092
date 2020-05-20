b6018504176395f5860c2ffcb94ee4d92d73230f
Muhamad-Rifqi <mrifqims13@gmail.com>
2020-05-21 00:37:00 +0700

    index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul>
+        <li><a href="kuliah/">Kuliah</a></li>
+    </ul>
+    <ul>
+        <li><a href="praktikum/">Praktikum</a></li>
+    </ul>
+    <ul>
+        <li><a href="tubes/">Tubes</a></li>
+    </ul>
+</body>
+
+</html>
